0b969e9a8cc91b5de67157d7d1c946432e513a11
Renan WT <renanwt@gmail.com>
2025-05-03 21:41:46 -0300

    Initial tests

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    render plain: "Welcome to the Email Sender API!!! To send emails, make a POST request to /send_message with a JSON body like: { \"message\": \"Your email content.\", \"emails\": [\"recipient1@example.com\", \"recipient2@example.com\"] }"
+    render plain: "Welcome to the Email Sender API! To send emails, make a POST request to /send_message with a JSON body like: { \"message\": \"Your email content.\", \"emails\": [\"recipient1@example.com\", \"recipient2@example.com\"] }"
   end
 end
