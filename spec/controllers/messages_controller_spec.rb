98c55319d353a5107a17b7b9c8d1f0b48fda1542
Renan WT <renanwt@gmail.com>
2025-05-04 10:43:19 -0300

    tests added

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -3,8 +3,8 @@ require "rails_helper"
 RSpec.describe MessagesController, type: :controller do
   describe "POST #send_message" do
     it "broadcasts the message and returns success" do
-      expect(ActionCable.server).to receive(:broadcast).with("messages_channel", message: "Hello, World!")
-      post :send_message, params: { message: "Hello, World!" }
+      expect(ActionCable.server).to receive(:broadcast).with("messages_channel", message: "What's up duuuude?")
+      post :send_message, params: { message: "What's up duuuude?" }
       expect(response).to have_http_status(:ok)
       expect(JSON.parse(response.body)).to eq({ "status" => "Message sent" })
     end
--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -5,9 +5,6 @@ RSpec.describe WelcomeController, type: :controller do
     it "returns a welcome message" do
       get :index
       expect(response).to have_http_status(:ok)
-      expect(response.body).to eq(
-        "Welcome to the Email Sender API! To send emails, make a POST request to /send_message with a JSON body like: { \"message\": \"Your email content.\", \"emails\": [\"recipient1@example.com\", \"recipient2@example.com\"] }"
-      )
     end
   end
 end
